e7c8b8a8c2e019285d3979562fec81e83a6b6381
umbax <umbax@tiscalinet.it>
2017-01-19 00:52:48 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,8 +10,8 @@ int main (int argc, char *argv[])
 {
 	// check the number of arguments
 	if (argc!=2) {
-		cerr << "\nERRORE!!! Numero di argomenti sbagliato!"	;
-		cerr << "\nUSO: >> ./main  location/name_input"; // existing_directory_output";
+		cerr << "\nERRORE!!! Wrong number of arguments!"	;
+		cerr << "\nCorrect way: >> ./main  location/name_input"; // existing_directory_output";
 		cerr << "\nEsempio: >> ./main ./input/input_file.txt"; // ./output\n";
 		exit(-1);
 	}
@@ -23,17 +23,17 @@ int main (int argc, char *argv[])
 
 
 
-	cout << "\n Software selezione antivibranti per UTA McQuay a 4-6 appoggi" << endl;
+	cout << "\n BeamGrid" << endl;
 
 	// declare Machine
-	Machine UTA; // (total_weight, min_freq, isolation_c, components_no, components, mounts_no, mounts_position, mounts_type);
+	Machine U; // (total_weight, min_freq, isolation_c, components_no, components, mounts_no, mounts_position, mounts_type);
 
 	// read input file and assign member values
-	read_input_file(FILE_INPUT, &UTA);
+	read_input_file(FILE_INPUT, &U);
 
 	//UTA.print_members();
-	UTA.compute_mount_loads();
-	UTA.compute_required_deflection();
+	U.compute_mount_loads();
+	U.compute_required_deflection();
 
 
 
